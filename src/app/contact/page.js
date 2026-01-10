5b39ab4617dc70e9ad3b7cae71cf979e5312458c
Jose Imhoff <jose-imhoff@hotmail.com>
2026-01-10 20:02:38 -0300

    add: create contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,1 +1,8 @@
- 
+export default function ContactPage() {
+    return (
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Contacto</h1>
+        <p className="mt-2">Página en construcción.</p>
+      </main>
+    );
+  }
